852ce5f96904d84ece9b75d96e3b5dddb2835445
Runbin Chen <56813177+OTLorz2@users.noreply.github.com>
2023-04-09 15:41:01 -0400

    Slightly modifed sc.c
    Converted pow() calls to multiplication; Removed some nested 'if' statements; Added a MAX macro.

--- scratch/sc.c
+++ scratch/sc.c
@@ -1,7 +1,7 @@
 double euclidean_distance(double *p1, double *p2, int dim) {
     double distance = 0.0;
     for (int i = 0; i < dim; i++) {
-        distance += pow(p1[i] - p2[i], 2);
+        distance += (p1[i] - p2[i]) * (p1[i] - p2[i]);
     }
     return sqrt(distance);
 }
@@ -11,13 +11,11 @@ double average_cluster_distance(double **data, int *labels, int n, int dim, int 
     double distance_sum = 0.0;
     int count = 0;
     for (int i = 0; i < n; i++) {
-        if (labels[i] == cluster) {
-            for (int j = 0; j < n; j++) {
-                if (labels[j] == cluster && i != j) {
-                    distance_sum += euclidean_distance(data[i], data[j], dim);
-                    count++;
-                }
-            }
+        if (labels[i] != cluster) continue;
+        for (int j = 0; j < n; j++) {
+            if (labels[j] != cluster || i == j) continue;
+            distance_sum += euclidean_distance(data[i], data[j], dim);
+            count++;
         }
     }
     return distance_sum / count;
@@ -31,26 +29,24 @@ double average_nearest_cluster_distance(double **data, int *labels, int n, int d
     double distance_sum = 0.0;
     int count = 0;
     for (int i = 0; i < n; i++) {
-        if (labels[i] != cluster) {
-            double distance = average_cluster_distance(data, labels, n, dim, labels[i]);
-            distance_sum += distance;
-            count++;
-        }
+        if (labels[i] == cluster) continue;
+        double distance = average_cluster_distance(data, labels, n, dim, labels[i]);
+        distance_sum += distance;
+        count++;
     }
     return distance_sum / count;
 }
 
 
+#ifndef MAX
+#define MAX(x, y) ((x < y) ? y : x)
+#endif
 
 double silhouette_coefficient(double **data, int *labels, int n, int dim, int point) {
     int cluster = labels[point];
     double a = average_cluster_distance(data, labels, n, dim, cluster);
     double b = average_nearest_cluster_distance(data, labels, n, dim, cluster);
     double s;
-    if(a>=b)
-        s = (b - a) / a;
-    else
-        s = (b - a) / b;
+    s = (b - a) / MAX(a, b);
     return s;
 }
-
