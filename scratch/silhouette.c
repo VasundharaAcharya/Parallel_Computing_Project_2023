f5df8f20773f3d24a23466c302f80b798c6b80de
Runbin Chen <56813177+OTLorz2@users.noreply.github.com>
2023-04-09 21:12:57 -0400

    Add files via upload

---
+++ scratch/silhouette.c
@@ -0,0 +1,56 @@
+double euclidean_distance(double *p1, double *p2, int dim) {
+    double distance = 0.0;
+    for (int i = 0; i < dim; i ++ ) {
+        distance += (p1[i] - p2[i]) * (p1[i] - p2[i]);
+    }
+    return sqrt(distance);
+}
+
+
+// Calculate the average distance for the data point with index 'idx' in 'data'
+//  within its cluster
+double average_cluster_distance(double **data, int *labels, int numdata, int dim, int idx) {
+    double distance_sum = 0.0;
+    int count = 0;
+    for (int i = 0; i < numdata; i ++ ) {
+        if (labels[i] != labels[idx]) continue;
+        distance_sum += euclidean_distance(data[idx], data[i], dim);
+        count ++ ;
+    }
+    return distance_sum / count;
+}
+
+
+#ifndef MIN
+#define MIN(x, y) ((x < y) ? x : y)
+#endif
+
+// Calculate the average nearest cluster distance for the data point with index 'idx' in 'data'
+//  to its nearest cluster
+// argument 'k' is the number of clusters
+double average_nearest_cluster_distance(double **data, int *labels, int numdata, int k, int dim, int idx) {
+    double cluster_distance[k] = {0.0};
+    double count[k] = {0};
+    for (int i = 0; i < numdata; i ++ ) {
+        cluster_distance[labels[i]] += euclidean_distance(data[idx], data[i], dim);
+        count[labels[i]] ++ ;
+    }
+    double res = 1e9;
+    for (int i = 0; i < k; i ++ ) {
+        if (i == labels[idx]) continue;
+        res = MIN(res, cluster_distance[i] / count[i]);
+    }
+    return res;
+}
+
+
+#ifndef MAX
+#define MAX(x, y) ((x < y) ? y : x)
+#endif
+
+// Calculate silhouette coefficient for the data point with index 'idx' in 'data'
+double silhouette_coefficient(double **data, int *labels, int numdata, int dim, int idx) {
+    double a = average_cluster_distance(data, labels, numdata, dim, idx);
+    double b = average_nearest_cluster_distance(data, labels, numdata, dim, idx);
+    return (b - a) / MAX(a, b);
+}
