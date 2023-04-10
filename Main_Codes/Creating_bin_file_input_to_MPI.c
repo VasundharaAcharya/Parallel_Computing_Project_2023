d07a4e7f0f3eb8c5dde29f579895ab3985b92999
VasundharaAcharya <78989055+VasundharaAcharya@users.noreply.github.com>
2023-04-10 13:38:57 -0400

    Update Creating_bin_file_input_to_MPI.c

--- Main_Codes/Creating_bin_file_input_to_MPI.c
+++ Main_Codes/Creating_bin_file_input_to_MPI.c
@@ -1,1 +1,47 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+//Change these as per the requirement of the project datasets
+#define rows 7129
+#define cols 34
+int main()
+{
+    // Open the text file for reading
+    FILE *fp_in = fopen("training.txt", "r");
+    if (fp_in == NULL) {
+        printf("Error: Unable to open the input file\n");
+        return 1;
+    }
+
+    // Open the binary file for writing
+    FILE *fp_out = fopen("output.bin", "wb");
+    if (fp_out == NULL) {
+        printf("Error: Unable to open the output file\n");
+        return 1;
+    }
+
+    // Read and write the data
+    // The data of the genes we read in is of type double 
+    double data;
+ 
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (fscanf(fp_in, "%lf", &data) == 1) {
+                fwrite(&data, sizeof(double), 1, fp_out);
+            } else {
+                printf("Error: Unable to read the input file\n");
+                fclose(fp_in);
+                fclose(fp_out);
+              exit(-1);
+            }
+        }
+    }
+
+    // Close the files
+    fclose(fp_in);
+    fclose(fp_out);
+
+    printf("Binary file is created!\n");
+
+    return 0;
+}
