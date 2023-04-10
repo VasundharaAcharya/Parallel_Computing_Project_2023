7a98d71d5d6615aace141309b002882889548ebb
Runbin Chen <56813177+OTLorz2@users.noreply.github.com>
2023-04-09 21:24:00 -0400

    Updated silhouette.c
    Included necessary header files, now it can compile in terminal successfully.

--- scratch/silhouette.c
+++ scratch/silhouette.c
@@ -1,3 +1,14 @@
+/**
+ * compile from terminal/shell as follows:
+ * 
+ *   bash$ gcc -Wall -Werror silhouette.c
+ * 
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
 double euclidean_distance(double *p1, double *p2, int dim) {
     double distance = 0.0;
     for (int i = 0; i < dim; i ++ ) {
@@ -29,8 +40,10 @@ double average_cluster_distance(double **data, int *labels, int numdata, int dim
 //  to its nearest cluster
 // argument 'k' is the number of clusters
 double average_nearest_cluster_distance(double **data, int *labels, int numdata, int k, int dim, int idx) {
-    double cluster_distance[k] = {0.0};
-    double count[k] = {0};
+    double cluster_distance[k];
+    memset(cluster_distance, 0.0, sizeof cluster_distance);
+    int count[k];
+    memset(count, 0, sizeof count);
     for (int i = 0; i < numdata; i ++ ) {
         cluster_distance[labels[i]] += euclidean_distance(data[idx], data[i], dim);
         count[labels[i]] ++ ;
@@ -49,8 +62,10 @@ double average_nearest_cluster_distance(double **data, int *labels, int numdata,
 #endif
 
 // Calculate silhouette coefficient for the data point with index 'idx' in 'data'
-double silhouette_coefficient(double **data, int *labels, int numdata, int dim, int idx) {
+double silhouette_coefficient(double **data, int *labels, int numdata, int k, int dim, int idx) {
     double a = average_cluster_distance(data, labels, numdata, dim, idx);
     double b = average_nearest_cluster_distance(data, labels, numdata, k, dim, idx);
     return (b - a) / MAX(a, b);
 }
+
+int main() { return EXIT_SUCCESS; };
