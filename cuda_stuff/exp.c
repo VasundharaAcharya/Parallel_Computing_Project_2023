9e1a442362b55e3969c4d89d77c3b6e285402ad5
Harshaa Hiremagalur Narayan <54905821+hhn20@users.noreply.github.com>
2023-04-20 12:19:43 -0400

    Update exp.c

--- cuda_stuff/exp.c
+++ cuda_stuff/exp.c
@@ -46,113 +46,113 @@ double *medoids;   					 // pointer to data  which stores the index of the centr
 /* INFINITY is supported */
 #endif
 
-// void computeMedoids(double* data, int* labels, double* medoids, int rank, int size);
-// void findclosestmedoids(double *data, double *medoids, int *idx, int rank, int size,int process_job, int si,int ei);
+void computeMedoids(double* data, int* labels, double* medoids, int rank, int size);
+void findclosestmedoids(double *data, double *medoids, int *idx, int rank, int size,int process_job, int si,int ei);
 // void computeMedoids(double* data, int* labels, double* medoids, int rank, int size);
 // void findclosestmedoids(double *data, double *medoids, int * , int rank, int process_job,int size, int si,int ei);
 //Finding the closeset medoids
 //This function works totally fine
-void findclosestmedoids(double *num, double *medoids, int *idx, int rank, int process_job,int size,int si,int ei) {
-    int i, j, l, for_i;
-    double sum, dist[K], min_dist, local_min_dist;
-    // Broadcast the medoids to all processes
-    MPI_Bcast(medoids, K * Samples, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+// void findclosestmedoids(double *num, double *medoids, int *idx, int rank, int process_job,int size,int si,int ei) {
+//     int i, j, l, for_i;
+//     double sum, dist[K], min_dist, local_min_dist;
+//     // Broadcast the medoids to all processes
+//     MPI_Bcast(medoids, K * Samples, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    // Find the closest medoid for each local data point
-    for (for_i = 0; for_i <process_job; for_i++) {
-        i = si + for_i;
-        local_min_dist = INFINITY;
+//     // Find the closest medoid for each local data point
+//     for (for_i = 0; for_i <process_job; for_i++) {
+//         i = si + for_i;
+//         local_min_dist = INFINITY;
 
-        for (j = 0; j < K; j++) {
-            sum = 0;
+//         for (j = 0; j < K; j++) {
+//             sum = 0;
 
-            for (l = 0; l < Samples; l++) {
+//             for (l = 0; l < Samples; l++) {
              
-                sum += fabs(num[i * Samples + l] - medoids[j * Samples + l]);
-            }
+//                 sum += fabs(num[i * Samples + l] - medoids[j * Samples + l]);
+//             }
 
-            dist[j] = sum;
+//             dist[j] = sum;
 
-            if (MIN1(dist[j],local_min_dist)) {
-                local_min_dist = dist[j];
-                idx[i] = j;
-            }
-        }
-
-
-    }
+//             if (MIN1(dist[j],local_min_dist)) {
+//                 local_min_dist = dist[j];
+//                 idx[i] = j;
+//             }
+//         }
 
 
+//     }
 
-    // Reduce the local min distances to find the global min distance
-    //The reason why we use
-    MPI_Allreduce(&local_min_dist, &min_dist, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
 
-    // Update the indices for data points with the global min distance
-    for (i = si; i < ei; i++) {
-        if (dist[idx[i]] == min_dist) {
-            idx[i] = j;
-        }
-    }
-}
+
+//     // Reduce the local min distances to find the global min distance
+//     //The reason why we use
+//     MPI_Allreduce(&local_min_dist, &min_dist, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
+
+//     // Update the indices for data points with the global min distance
+//     for (i = si; i < ei; i++) {
+//         if (dist[idx[i]] == min_dist) {
+//             idx[i] = j;
+//         }
+//     }
+// }
 
 
 
 
 
 // //This code is same as the serial code, but the difference is the splitting of the clusters among processes
-void computeMedoids(double* gene_data, int* idx, double* medoids, int rank, int size) {
-    int i, j, k, l, m;
-    double min_distance, distance, sum, temp;
+// void computeMedoids(double* gene_data, int* idx, double* medoids, int rank, int size) {
+//     int i, j, k, l, m;
+//     double min_distance, distance, sum, temp;
 
-    // using entire gene data instead of sampling
-    int sample_size = Genes; 
+//     // using entire gene data instead of sampling
+//     int sample_size = Genes; 
    
-    // Divide the K clusters across the processes
-    int medoids_per_proc = K / size;
-    int remainder = K % size;
-    int start_idx = rank * medoids_per_proc;
-    int end_idx = start_idx + medoids_per_proc;
+//     // Divide the K clusters across the processes
+//     int medoids_per_proc = K / size;
+//     int remainder = K % size;
+//     int start_idx = rank * medoids_per_proc;
+//     int end_idx = start_idx + medoids_per_proc;
    
-    if (rank == size - 1) {
-        end_idx += remainder;
-    }
+//     if (rank == size - 1) {
+//         end_idx += remainder;
+//     }
 
-    //printf("Process %d: start_idx = %d, end_idx = %d\n", rank, start_idx, end_idx);
+//     //printf("Process %d: start_idx = %d, end_idx = %d\n", rank, start_idx, end_idx);
 
-    // Seed the random number generator with the rank
-    srand(time(0) + rank);
+//     // Seed the random number generator with the rank
+//     srand(time(0) + rank);
 
-    for (i = start_idx; i < end_idx; i++) {
-        min_distance = 1e9;
+//     for (i = start_idx; i < end_idx; i++) {
+//         min_distance = 1e9;
 
-        // Calculate distances for all points in the cluster
-        for (j = 0; j < Genes; j++) {
-            if (idx[j] == i) {
-                sum = 0.0;
-                for (k = 0; k < Genes; k++) {
-                    if (idx[k] == i) {
-                        distance = 0.0;
-                        for (l = 0; l < Samples; l++) {
-                            // manhattan distance
-                            double diff = *(gene_data + j * Samples + l) - *(gene_data + k * Samples + l);
-                            distance += fabs(diff);
-                        }
-                        sum += distance;
-                    }
-                }
-//I am using the macro i defined for this
-                if (MIN1(sum, min_distance)) {
-                    min_distance = sum;
-                    for (m = 0; m < Samples; m++) {
-                        temp = *(gene_data + j * Samples + m);
-                        *(medoids + i * Samples + m) = temp;
-                    }                    
-                }
-            }
-        }
-    }
-}
+//         // Calculate distances for all points in the cluster
+//         for (j = 0; j < Genes; j++) {
+//             if (idx[j] == i) {
+//                 sum = 0.0;
+//                 for (k = 0; k < Genes; k++) {
+//                     if (idx[k] == i) {
+//                         distance = 0.0;
+//                         for (l = 0; l < Samples; l++) {
+//                             // manhattan distance
+//                             double diff = *(gene_data + j * Samples + l) - *(gene_data + k * Samples + l);
+//                             distance += fabs(diff);
+//                         }
+//                         sum += distance;
+//                     }
+//                 }
+// //I am using the macro i defined for this
+//                 if (MIN1(sum, min_distance)) {
+//                     min_distance = sum;
+//                     for (m = 0; m < Samples; m++) {
+//                         temp = *(gene_data + j * Samples + m);
+//                         *(medoids + i * Samples + m) = temp;
+//                     }                    
+//                 }
+//             }
+//         }
+//     }
+// }
 
 
 
@@ -166,6 +166,7 @@ int main(int argc, char *argv[]){
           int myrank, numranks, result;
           int i,j,k;
           int process_job, each_chunk_pos;
+          FILE *fp, *fw;
           double starttime, endtime;
 
           MPI_Init(NULL, NULL);
@@ -183,8 +184,8 @@ int main(int argc, char *argv[]){
           MPI_Request request1, request2, request3, request4; 
           double num1;
           MPI_File fh,fh1;
-          FILE *fp;
           int si,ei;
+
 
           //Determination of each chunk of the input file each process is going to read
           process_job=Genes/world_size;
@@ -205,56 +206,88 @@ int main(int argc, char *argv[]){
 
 
 /* MPI I/O to read from the bin file created*/
-          int resultant;
+        //   int resultant;
           //For this we need to have the bin file rather than txt file.
 
-          resultant = MPI_File_open(MPI_COMM_WORLD, "input.bin", MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
-          if (resultant != MPI_SUCCESS) {
-              printf("Error opening file. \n");
-              exit(-1);
-          }
+        //   resultant = MPI_File_open(MPI_COMM_WORLD, "input.bin", MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
+        //   if (resultant != MPI_SUCCESS) {
+        //       printf("Error opening file. \n");
+        //       exit(-1);
+        //   }
 
 
 
 
-          // Determining the file size is the most important step as I experimented with partial filesize led to wrong results
-          MPI_Offset file_size;
-          MPI_File_get_size(fh, &file_size);
+        //   // Determining the file size is the most important step as I experimented with partial filesize led to wrong results
+        //   MPI_Offset file_size;
+        //   MPI_File_get_size(fh, &file_size);
 
-         //This is taken from slide number 11 of mpi I/O
-          MPI_Offset buffsize = file_size / world_size;
-          MPI_Offset start_idx = world_rank * buffsize;
-          MPI_Offset end_idx =world_rank * buffsize + buffsize;
-          if (world_rank == world_size - 1) {
-              end_idx = file_size;
+        //  //This is taken from slide number 11 of mpi I/O
+        //   MPI_Offset buffsize = file_size / world_size;
+        //   MPI_Offset start_idx = world_rank * buffsize;
+        //   MPI_Offset end_idx =world_rank * buffsize + buffsize;
+        //   if (world_rank == world_size - 1) {
+        //       end_idx = file_size;
 
-          }
-          MPI_Offset local_size =world_rank * buffsize + buffsize -world_rank * buffsize ;
+        //   }
+        //   MPI_Offset local_size =world_rank * buffsize + buffsize -world_rank * buffsize ;
 
-          double* local_data = (double*)malloc(local_size * sizeof(double));
+        //   double* local_data = (double*)malloc(local_size * sizeof(double));
 
-          // This is taken from the slide number 20 of the mpi I/O slide
-         // MPI_File_set_view(fh,  world_rank * buffsize, MPI_DOUBLE, MPI_DOUBLE, "native", MPI_INFO_NULL);
-
-
-          MPI_File_read(fh, local_data, local_size, MPI_DOUBLE, &status);
-
-          MPI_File_close(&fh);
+        //   // This is taken from the slide number 20 of the mpi I/O slide
+        //  // MPI_File_set_view(fh,  world_rank * buffsize, MPI_DOUBLE, MPI_DOUBLE, "native", MPI_INFO_NULL);
 
 
+        //   MPI_File_read(fh, local_data, local_size, MPI_DOUBLE, &status);
+
+        //   MPI_File_close(&fh);
+
+        //   printf("hello\n");
+
+// MPI_Barrier(MPI_COMM_WORLD);
 
 
-
-
-for (i = 0; i < Genes; i++) {
-    for (j = 0; j < Samples; j++) {
-        *(gene_data + i * Samples + j) = *(local_data + i * Samples + j);
+if(world_rank==0)
+{
+    fp = fopen("input.bin", "rb");
+    if (fp == NULL) {
+        printf("The requested input file does not exist. \n");
+        exit(-1);
     }
+
+
+
+
+
+
+   for (i=0;i<Genes;i++){
+		for (j=0;j<Samples;j++){
+            fread(&num1,sizeof(double),1,fp);
+			// fread(fp,"%lf", &num1);
+			*(gene_data+i*Samples+j)=num1;
+		}
+	}
+
+
+
+
+	fclose(fp);
+
+
 }
 
 
+MPI_Barrier(MPI_COMM_WORLD);
 
-free(local_data);
+
+// for (i = 0; i < Genes; i++) {
+//     for (j = 0; j < Samples; j++) {
+//         *(gene_data + i * Samples + j) = *(local_data + i * Samples + j);
+//     }
+// }
+
+
+// free(local_data);
 
   	if (world_rank==0)
 	{	
@@ -282,7 +315,6 @@ free(local_data);
   }
 MPI_Barrier(MPI_COMM_WORLD);
 
-   
  /*This is for the findclosestmedoids function. We have computed the starting and ending indexes separately 
  for the computeMedoids function inside the function.*/
 
@@ -302,7 +334,6 @@ MPI_Barrier(MPI_COMM_WORLD);
  The broadcast to all the processes and also the computation of final result using MPI_Allreduce is done in
  the function itself. Therefore we do not do it here. But we meed to do all gather to gather the results from all processes after
  the computeMed function */
-
 
 
 	//MPI_Bcast(medoids, K*Samples, MPI_DOUBLE, 0, MPI_COMM_WORLD);
